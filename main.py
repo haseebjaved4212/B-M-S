fcdb138f98504417da886adcaa141f848e19080a
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-11 16:41:23 +0500

    feat: implement menu interface for banking management system operations

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Press 1 for Creating an Account")
+print("Press 2 for Depositing Money in the Bank Account")
+print("Press 3 for Withdrawing Money from Bank Account")
+print("Press 4 for Checking Balance in Bank Account")
+print("Press 5 for Creating Fixed Deposit")
+print("Press 6 for Checking Maturity Date of Fixed Deposit")
+print("Press 7 for Withdrawing Fixed Deposit")
+print("Press 8 for Deleting Account")
+
+
+        
