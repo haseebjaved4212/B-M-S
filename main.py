296578c5d176fc677d60bf4485043f4abe8a32da
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-11 16:45:29 +0500

    feat: add console menu interface for banking management system operations

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Press 1 for Creating an Account")
+print("Press 2 for Depositing Money in the Bank Account")
+print("Press 3 for Withdrawing Money from Bank Account")
+print("Press 4 for Checking details of Bank Account")
+print("Press 5 for Updating the details")
+print("Press 6 for Deleting the Account")
